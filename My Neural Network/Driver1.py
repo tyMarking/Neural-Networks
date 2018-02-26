ffac96475c8d48c143fde6c8a83512c88706b2e5
Ty Marking <136029@nmusd.us>
2018-02-26 11:46:52 -0800

    added evaluating and saving

--- My Neural Network/Driver1.py
+++ My Neural Network/Driver1.py
@@ -5,12 +5,56 @@ Created on Sat Feb 24 14:16:22 2018
 @author: Ty
 """
 import MyNeuralNet as NN
+import gzip
+import json
+import numpy as np
 
-dimensions = (784,16,16)
-print(NN.newNet(dimensions))
-file1 = open("data2/train-labels-idx1-ubyte.gz", 'rb')
+
+#Helper functions
+def saveToFile(net, file):
+    netList = []
+    for layer in net:
+        netList.append((layer[0].tolist(),layer[1].tolist()))
+    netJson = json.dumps(netList)
+    file = open(file, "w")
+    file.truncate(0)
+    file.write(netJson)
+
+def loadFromFile(file):
+    file = open(file, "r")
+    netJson = file.read()
+    netList = json.loads(netJson)
+    matrixList = []
+    for layer in netList:
+        matrixList.append((np.matrix(layer[0]),np.matrix(layer[1])))
+    return matrixList
+#create the inital net
+dimensions = (728,16,16,10)
+#net = NN.newNet(dimensions)
+net = loadFromFile("firstNet.txt")
+#read the MNIST data
+print("Reading MNIST data")
+trainImages = gzip.open("data/train-images-idx3-ubyte.gz", 'rb')
+trainLabels = gzip.open("data/train-labels-idx1-ubyte.gz", 'rb')
 imagesBytes = []
-print(file1.read(36))
-for b in file1.read():
-    imagesBytes.append(b)
-print(imagesBytes[0:64])
+
+
+trainImages.read(16)
+trainLabels.read(8)
+trainData = []
+
+for i in range(60000):
+    image = []
+    for pixle in trainImages.read(728):
+        image.append(pixle/255)
+    label = trainLabels.read(1)[0]
+    trainData.append((image, label))
+
+print("Finished reading MNIST data")
+
+print(NN.run(trainData[0][0], net))
+print(trainData[0][1])
+
+
+
+saveToFile(net, "firstNet.txt")
--- My Neural Network/MyNeuralNet.py
+++ My Neural Network/MyNeuralNet.py
@@ -19,6 +19,17 @@ dimensions as (inputs, L1, L2... Ln, outputs)
 import numpy as np
 import random
 
+import math
+#helper function
+def sigmoid(x):
+    xList = x.tolist()
+    newList = []
+    for subList in xList:
+        newSubList = []
+        for item in subList:
+            newSubList.append(1 / (1 + math.exp(-item)))
+        newList.append(newSubList)
+    return np.matrix(newList)
 
 """ New Net
 precondition: dimensions is an array of form (inputs, L1, L2... Ln, outputs). Length must be at least 3
@@ -38,18 +49,34 @@ def newNet(dimensions):
         #forming weight matrix
         rands = []
         for j in range(prevNodes*dim):
-            rands.append(random.gauss(0, 3))
+            rands.append(random.gauss(0, 0.5))
         rands = np.reshape(rands, (dim,prevNodes))
         weights = np.matrix(rands)
         
         #forming biase matrix
         rands = []
         for j in range(dim):
-            rands.append(random.gauss(0, 3))
+            rands.append(random.gauss(0, 0.5))
         rands = np.reshape(rands, (dim,1))
         biases = np.matrix(rands)
         
         #adding (weights, biases) to layers
         layers.append((weights,biases))
-    
+    print("New neural net generated")
     return layers
+
+"""
+precondition: inputs is a list of the first activations
+
+"""
+def run(inputs, net):
+    #1: multiply weights by prev activations
+    #2: apply non-linear (sigmoid or Relu)
+    #3: repeat 1-2 for all layers
+    #4: return last activation
+    a = np.reshape(inputs, (len(inputs),1))
+    a = np.matrix(a)
+    for layer in net:
+        a = sigmoid(layer[0]*a+layer[1])
+    return a
+    
